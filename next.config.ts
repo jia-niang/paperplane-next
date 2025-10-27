44e2ac6a030cb6ec591e1ca05e9bbc89b9b53dcb
jia-niang <1@paperplane.cc>
2025-10-27 16:44:58 +0800

    chore(app): 现在 Next.js 配置使用 .ts 文件；补全缺失的工具链 types

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
 import createMDX from '@next/mdx'
+import type { NextConfig } from 'next'
 import remarkPrism from 'remark-prism'
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   output: 'standalone',
   assetPrefix:
     process.env.NODE_ENV === 'production' ? 'https://cdn.paperplane.cc/paperplane-next' : undefined,
   productionBrowserSourceMaps: process.env.NODE_ENV === 'development',
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   images: {
-    remotePatterns: [{ protocol: 'https', hostname: process.env.NEXT_PUBLIC_SHIELDS_HOST }],
+    remotePatterns: [{ protocol: 'https', hostname: process.env.NEXT_PUBLIC_SHIELDS_HOST! }],
     loader: process.env.NODE_ENV === 'production' ? 'custom' : undefined,
     loaderFile: process.env.NODE_ENV === 'production' ? './misc/image-loader.js' : undefined,
     dangerouslyAllowSVG: true,
@@ -24,11 +24,7 @@ const nextConfig = {
 const withMDX = createMDX({
   extension: /\.mdx?$/,
   options: {
-    remarkPlugins: [remarkPrism],
-  },
-  mdxOptions: {
-    remarkPlugins: [],
-    rehypePlugins: [],
+    remarkPlugins: [remarkPrism as any],
   },
 })
 
