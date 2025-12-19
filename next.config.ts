b7101a4f19afec4eaeabd5277e74413569fd7be8
jia-niang <1@paperplane.cc>
2025-12-20 06:04:17 +0800

    chore(cdn): 现在可以通过环境变量配置图片 CDN，且 image-loader 仅在配置了 CDN 后才生效

--- next.config.ts
+++ next.config.ts
@@ -5,13 +5,21 @@ import remarkPrism from 'remark-prism'
 const nextConfig: NextConfig = {
   output: 'standalone',
   assetPrefix:
-    process.env.NODE_ENV === 'production' ? 'https://cdn.paperplane.cc/paperplane-next' : undefined,
+    process.env.NODE_ENV === 'production'
+      ? process.env.NEXT_PUBLIC_CDN_BASE_URL || undefined
+      : undefined,
   productionBrowserSourceMaps: process.env.NODE_ENV === 'development',
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   images: {
     remotePatterns: [{ protocol: 'https', hostname: process.env.NEXT_PUBLIC_SHIELDS_HOST! }],
-    loader: process.env.NODE_ENV === 'production' ? 'custom' : undefined,
-    loaderFile: process.env.NODE_ENV === 'production' ? './misc/image-loader.js' : undefined,
+    loader:
+      process.env.NODE_ENV === 'production' && process.env.NEXT_PUBLIC_CDN_BASE_URL
+        ? 'custom'
+        : undefined,
+    loaderFile:
+      process.env.NODE_ENV === 'production' && process.env.NEXT_PUBLIC_CDN_BASE_URL
+        ? './misc/image-loader.js'
+        : undefined,
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
