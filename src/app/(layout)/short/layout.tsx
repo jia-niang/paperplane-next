152320750b0bb00a3970a5f2f52d87e6edccc8de
jia-niang <1@paperplane.cc>
2026-01-12 09:00:01 +0800

    feat(short): 为 short 页面预获取用户登录状态

--- src/app/(layout)/short/layout.tsx
+++ src/app/(layout)/short/layout.tsx
@@ -1,12 +1,19 @@
+import { prefetchSession } from '@daveyplate/better-auth-tanstack/server'
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
+import { headers } from 'next/headers'
 
+import { auth } from '@/lib/auth'
 import { getQueryClient } from '@/lib/query-client'
 import { trpcServer } from '@/lib/trpc-server'
 
 export default async function ShortLayout({ children }: LayoutProps<'/short'>) {
   const queryClient = getQueryClient()
 
-  await Promise.all([queryClient.prefetchQuery(trpcServer.short.items.list.queryOptions())])
+  await Promise.all([
+    prefetchSession(auth, queryClient, { headers: await headers() }),
+
+    queryClient.prefetchQuery(trpcServer.short.items.list.queryOptions()),
+  ])
 
   return <HydrationBoundary state={dehydrate(queryClient)}>{children}</HydrationBoundary>
 }
