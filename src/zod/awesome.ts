213613476f0014861202a6a2d337751d55a73319
jia-niang <1@paperplane.cc>
2026-01-16 14:50:30 +0800

    fix(awesome): 现在 URL 类表单输入兼容空字符串

--- src/zod/awesome.ts
+++ src/zod/awesome.ts
@@ -7,8 +7,8 @@ export const awesomeItemZod = z.object({
 
   label: z.string('Awesome 名称是必须的').min(2, '名称至少需要 2 个字符'),
   homepage: z.url('主页地址格式不正确'),
-  source: z.url('源代码仓库地址格式不正确').nullish().default(''),
-  registry: z.url('制品地址格式不正确').nullish().default(''),
+  source: z.url('源代码仓库地址格式不正确').or(z.literal('')).nullish().default(''),
+  registry: z.url('制品地址格式不正确').or(z.literal('')).nullish().default(''),
   desc: z.string().nullish().default(''),
   stars: z.number().min(0).max(5).nullish(),
 
